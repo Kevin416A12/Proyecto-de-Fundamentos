61ea7f9321bea135e34b37747fc40685271dba8f
Elanion668 <jacobzumbado@gmail.com>
2024-10-13 13:34:27 -0600

    inicio interfaz

---
+++ interfaz.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+def ventana_principal():
+    inicio = Tk()
+    inicio.title('PinBall')
+    inicio.config(bg='black')
+    inicio.minsize()
+    inicio.mainloop()
+    logo= Label
+ventana_principal()
